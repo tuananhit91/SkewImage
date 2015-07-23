3d7cba9df726c88b878e70946a7949e6daae5887
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-07-23 18:01:02 +0700

    update
    update

--- SkewImage/ViewController.m
+++ SkewImage/ViewController.m
@@ -9,15 +9,69 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *iPhoto;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
 
 @end
 
 @implementation ViewController
-
+NSTimer* _timer;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.05
+                                              target:self
+                                            selector:@selector(onTimer)
+                                            userInfo:nil
+                                             repeats:YES];
+    
+    self.slider.tintColor = [UIColor orangeColor];
+    self.slider.thumbTintColor = [UIColor blueColor];
 }
+
+- (void)onTimer{
+    self.slider.value += 0.01;
+    if (self.slider.value == 1) {
+        self.slider.value = 0;
+    }else if((0<self.slider.value) && (self.slider.value < 0.5)){
+        CATransform3D t = CATransform3DIdentity;
+        t.m34 = -0.001;
+        t = CATransform3DRotate(t, M_PI / (0.3 + self.slider.value), 0.0, 1.0, 0.0);
+        self.iPhoto.layer.transform = t;
+    }else{
+        CATransform3D t = CATransform3DIdentity;
+        t.m34 = -0.001;
+        t = CATransform3DRotate(t, M_PI / -(self.slider.value), 0.0, 1.0, 0.0);
+        self.iPhoto.layer.transform = t;
+    }
+}
+
+- (IBAction)ONOFFSlider:(id)sender {
+    if (_timer) {
+        [_timer invalidate];
+        _timer = nil;
+    }else{
+        _timer = [NSTimer scheduledTimerWithTimeInterval:0.1
+                                                  target:self
+                                                selector:@selector(onTimer)
+                                                userInfo:nil
+                                                 repeats:YES];
+    }
+}
+
+- (IBAction)actionSlider:(id)sender {
+    if((0<self.slider.value) && (self.slider.value < 0.5)){
+        CATransform3D t = CATransform3DIdentity;
+        t.m34 = -0.001;
+        t = CATransform3DRotate(t, M_PI / (0.3 + self.slider.value), 0.0, 1.0, 0.0);
+        self.iPhoto.layer.transform = t;
+    }else{
+        CATransform3D t = CATransform3DIdentity;
+        t.m34 = -0.001;
+        t = CATransform3DRotate(t, M_PI / -(self.slider.value), 0.0, 1.0, 0.0);
+        self.iPhoto.layer.transform = t;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
